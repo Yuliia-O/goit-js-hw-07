2faa3eebcb3f01788ae76e10578f0621f2046369
Yuliia Ovsiannikova <yuliia.ovsiannikova@outlook.com>
2022-07-17 23:34:32 +0200

    Completed task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+function doImgMarkup(array) {
+    return array.reduce(
+        (acc, item) =>
+            acc +
+            `<a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}"/></a>`,
+        '',
+    );
+}
+
+const imgMarkup = doImgMarkup(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML('afterbegin', imgMarkup);
+
+function galleryHandler(event) {
+    event.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+    lightbox.open();
+}
+
+galleryEl.addEventListener('click', galleryHandler);
